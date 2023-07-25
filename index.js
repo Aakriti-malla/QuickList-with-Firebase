14822733ec209bc9c93ba2744b740fb816be8789
Aakriti-malla <aakritimalla91@gmail.com>
2023-07-25 20:08:47 +0530

    Firebase Realtime DB connection

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const inputField = document.getElementById("input-field");
-const addButton = document.getElementById("add-button");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import config from "./config.js";
 
-addButton.addEventListener("click", function () {
-  let inputValue = inputField.value;
-  console.log(inputValue);
+const appSettings = {
+  ...config,
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const List = ref(database, "QuickList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(List, inputValue);
+
+  console.log(`${inputValue} added to database`);
 });
